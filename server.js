927d1b9ee4dc147d9864da6f60ed3bfd924ec480
Brian Griffin <wbgriffin@gmail.com>
2020-11-22 21:18:13 -0500

    Create /exploit endpoint

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 
-const app = express();
 const port = process.env.PORT || 5000;
-
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
 
+app.post('/exploit', (req, res) => {
+  console.log(req.body);
+  const payload = { success: true };
+  res.send(payload);
+});
+
+app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
